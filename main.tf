740bd8580f5a3f3bbc1f57230f6be4c38d3f17f6
akshay latthe <124586197+akshay7276427289@users.noreply.github.com>
2023-06-16 13:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAVYYASOQZOHY3LD6R"
-  secret_key = "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"
+  access_key = 
+  secret_key = 
 } 
 
 
